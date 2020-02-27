9cd1d7592bd9cd59d0a51ae9975d311669705fc6
Dannie Schumaker <draeschumaker@gmail.com>
2020-02-27 10:17:57 -0800

    add auth routes to server.js

--- server.js
+++ server.js
@@ -21,6 +21,36 @@ app.use(express.json());
 // API Routes
 app.use(express.urlencoded({ extended: true }));
 
+// *** AUTH ***
+const createAuthRoutes = require('./lib/auth/create-auth-routes');
+
+const authRoutes = createAuthRoutes({
+    selectUser(email) {
+        return client.query(`
+        SELECT id, email, hash
+        FROM users
+        WHERE email = $1;`,
+        [email]
+        ).then(result => result.rows[0]);
+    },
+    insertUser(user, hash) {
+        return client.query(`
+        INSERT into users (email, hash)
+        VALUES ($1, $2)
+        RETURNING id, email;
+        `,
+        [user.email, hash]
+        ).then(result => result.rows[0]);
+    }
+});
+
+app.use('/api/auth', authRoutes);
+
+const ensureAuth = require('./lib/auth/ensure-auth');
+
+app.use('/api', ensureAuth);
+
+
 // *** TODOS ***
 // this is /GET request that returns whole list of todos
 app.get('/api/todos', async(req, res) => {
